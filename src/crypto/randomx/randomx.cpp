ef54b77bf22ad20d51ec0aef91966dbf9f8f4b17
kevacoin <info@kevacoin.org>
2020-03-02 14:58:38 -0800

    Reduced L2 scratchpad size by half.

--- src/crypto/randomx/randomx.cpp
+++ src/crypto/randomx/randomx.cpp
@@ -100,6 +100,7 @@ RandomX_ConfigurationSafex::RandomX_ConfigurationSafex()
 RandomX_ConfigurationKeva::RandomX_ConfigurationKeva()
 {
 	ArgonSalt = "RandomKV\x01";
+	ScratchpadL2_Size = 131072;
 	ScratchpadL3_Size = 1048576;
 }
 
