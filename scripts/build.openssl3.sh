3d30706845055bd528e51153f7e63633a7c9f896
BenDr0id <ben@graef.in>
2026-04-27 18:43:49 +0200

    Fix for crosscompile openssl

--- scripts/build.openssl3.sh
+++ scripts/build.openssl3.sh
@@ -12,7 +12,31 @@ wget https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERS
 tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
 
 cd openssl-${OPENSSL_VERSION}
-./config -no-shared -no-asm -no-zlib -no-comp -no-dgram -no-filenames -no-cms
+
+# On macOS, ./config follows the host OpenSSL sees and can still produce arm64
+# static libs under Rosetta when the app is built for x86_64. Use explicit
+# Configure targets when XMRIG_MACOSX_DEP_ARCH is set (CI) or infer from uname.
+OPTS='-no-shared -no-asm -no-zlib -no-comp -no-dgram -no-filenames -no-cms'
+if [ "$(uname -s)" = "Darwin" ]; then
+    DEP_ARCH="${XMRIG_MACOSX_DEP_ARCH:-}"
+    if [ -z "$DEP_ARCH" ]; then
+        DEP_ARCH=$(uname -m)
+    fi
+    case "$DEP_ARCH" in
+        x86_64|amd64)
+            ./Configure darwin64-x86_64-cc $OPTS
+            ;;
+        arm64|aarch64)
+            ./Configure darwin64-arm64-cc $OPTS
+            ;;
+        *)
+            ./config $OPTS
+            ;;
+    esac
+else
+    ./config $OPTS
+fi
+
 make -j$(nproc || sysctl -n hw.ncpu || sysctl -n hw.logicalcpu)
 cp -fr include ../../deps
 cp libcrypto.a ../../deps/lib
